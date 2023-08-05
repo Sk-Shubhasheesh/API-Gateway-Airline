fe4e4c174fe2c4b99fc87430781e40154d54b350
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-08-05 07:23:21 +0530

    Added proxy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
-const { ServerConfig } = require('./config');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
@@ -16,6 +17,16 @@ app.use(express.urlencoded({extended: true}));
 // Apply the rate limiting middleware to all requests
 app.use(limiter)
 
+
+app.use('/flightsService', createProxyMiddleware({ 
+	target: ServerConfig.FLIGHT_SERVICE, 
+	changeOrigin: true,
+    pathRewrite: {'^/flightsService' : '/'}
+ }));
+app.use('/bookingService', createProxyMiddleware({ 
+	target: ServerConfig.BOOKING_SERVICE, 
+	changeOrigin: true,
+	pathRewrite: {'^/bookingService' : '/'} }));
 app.use('/api', apiRoutes);
 
 
