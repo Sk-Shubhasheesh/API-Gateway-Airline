080e49579f74c98c1df2408fa5632e2bc4ab3edd
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2023-08-06 06:25:37 +0530

    added model for many to many assoc between roles and users

--- src/utils/common/enum.js
+++ src/utils/common/enum.js
@@ -1,10 +1,9 @@
-const SEAT_TYPE = {
-    BUSINESS: 'business',
-    ECONOMY: 'economy',
-    PREMIUM_ECONOMY: 'premium-economy',
-    FIRST_CLASS: 'first_class'
+USER_ROLES_ENUMS = {
+    ADMIN: 'admin',
+    CUSTOMER: 'customer',
+    FLIGHT_COMPANY: 'flight_company'
 }
 
 module.exports = {
-    SEAT_TYPE
+    USER_ROLES_ENUMS
 }
